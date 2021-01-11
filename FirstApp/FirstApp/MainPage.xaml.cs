1c7a7456773711afc3891d921b9a5877c30e6e14
FilipDavid1 <filipenkodavid@gmail.com>
2021-01-11 08:29:12 +0100

    Add project files.

---
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Button.Clicked += Button_Clicked;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string accosting = Entry.Text;
+            await DisplayAlert($"Hi, {accosting}!", "Welcome in your first app.", "OK");
+        }
+    }
+}
